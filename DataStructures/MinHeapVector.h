8ef2c889a620f39ed2d1cc8337f2a8300f020c65
DESKTOP-H30GHKU\Noxam <noxamsherei@gmail.com>
2022-02-10 09:33:55 +0100

    Commit Test

--- DataStructures/MinHeapVector.h
+++ DataStructures/MinHeapVector.h
@@ -4,7 +4,14 @@ template<class dataType=int>
 class MinHeapVector
 {
 public:
+	void addElement(dataType element)
+	{
 
+	}
+	bool addElement(dataType element)
+	{
+
+	}
 private:
 	std::vector<dataType> data;
 	size_t size;
